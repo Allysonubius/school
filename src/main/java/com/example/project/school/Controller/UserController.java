7c128ad1c253813d44e5ce415b0b91b20e264ae6
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2022-07-07 20:23:04 -0300

    Alteração na estrutura na resposta da requisção ao criar User

--- src/main/java/com/example/project/school/Controller/UserController.java
+++ src/main/java/com/example/project/school/Controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.project.school.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,25 +11,44 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import static java.util.Objects.isNull;
 import com.example.project.school.Entity.UserAuthEntity;
 import com.example.project.school.Entity.UserRegisterEntity;
 import com.example.project.school.Model.UserModel;
 import com.example.project.school.Service.UserService;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 @RestController
 @RequestMapping(path="/api",produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
     
-    //@Autowired
-    //private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+    @Autowired
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
 
+    private static Gson gson = new Gson();
+
     @PostMapping(path="/registerUser")
-    public ResponseEntity<UserAuthEntity> userRegisResponseEntity (@RequestBody UserRegisterEntity userRegisterEntity){
-        UserModel userModel = this.userService.userRegisteModel(userRegisterEntity.toUserRegister());
-        return new ResponseEntity<UserAuthEntity>(UserAuthEntity.toDTO(userModel, "Bearer "), HttpStatus.CREATED);
+    public ResponseEntity<Object> userRegisResponseEntity (@RequestBody UserRegisterEntity userRegisterEntity){
+        JsonObject messageJson = new JsonObject();
+        String message;
+        try {
+            UserModel userModel = this.userService.userRegisteModel(userRegisterEntity.toUserRegister());
+            JsonElement jsonElement = gson.toJsonTree(userModel);
+            messageJson.add("User", jsonElement);
+            
+            //return ResponseEntity<Object>(UserAuthEntity.toDTO(userModel, "Bearer "), HttpStatus.CREATED);
+            return ResponseEntity.status(HttpStatus.CREATED).body(messageJson.toString());
+        } catch (Exception e) {
+            LOG.error("ERROR", e);
+            message = e.getMessage() + (isNull(e.getCause()) ? "" : e.getCause().getMessage());
+            messageJson.add("error",new JsonPrimitive(message));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(messageJson.toString());
+        }
     }
 }
--- src/main/java/com/example/project/school/Service/TokenService.java
+++ src/main/java/com/example/project/school/Service/TokenService.java
@@ -1,6 +1,9 @@
 package com.example.project.school.Service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 import com.example.project.school.Model.UserModel;
 import io.jsonwebtoken.Claims;
@@ -15,9 +18,11 @@ public class TokenService {
     private static final long JWT_EXPIRATION_TIME = 1800000;
 
     public String generateBearerToken(UserModel userModel){
+        Map<String, Object> claims = new HashMap<>();
         return Jwts.builder()
+        .setClaims(claims)
         .setIssuedAt(new Date(System.currentTimeMillis()))
-        .setSubject("Teste JWT API")
+        .setSubject(userModel.getNome())
         .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_TIME))
         .signWith(SignatureAlgorithm.HS256, JWT_KEY)
         .compact();
